6e5682d29289a2c4104a1928db6fe04b3a2500eb
Chase MacMillan <chasemacmillan@hotmail.com>
2021-05-24 00:28:27 -0700

    first commit

---
+++ RollingShutter.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class RollingShutter : MonoBehaviour
+{
+    public Camera sceneCamera;
+    public Camera compositorCamera;
+    public Material maskMaterial;
+    public int temporalResolution = 100;
+    public RenderTextureFormat textureFormat = RenderTextureFormat.RGB565;
+
+    private CommandBuffer commandBuffer;
+    private List<RenderTexture> renderTextures = new List<RenderTexture>();
+
+    private void Start()
+    {
+        QualitySettings.vSyncCount = 2;
+        Application.targetFrameRate = 60;
+        int w = Screen.width;
+        int h = Screen.height;
+        for (int i = 0; i < temporalResolution; i++)
+        {
+            renderTextures.Add(new RenderTexture(w, h, 0, textureFormat, 0));
+        }
+        commandBuffer = new CommandBuffer();
+        commandBuffer.name = "Rolling shutter effect";
+        compositorCamera.AddCommandBuffer(CameraEvent.AfterEverything, commandBuffer);
+    }
+
+    void Update()
+    {
+        renderTextures.Insert(0, renderTextures[renderTextures.Count - 1]);
+        renderTextures.RemoveAt(renderTextures.Count - 1);
+        sceneCamera.targetTexture=renderTextures[0];
+
+        commandBuffer.Clear();
+        for (int i = 0; i < temporalResolution; i++)
+        {
+            commandBuffer.SetGlobalFloat("_MaskPercentage",(float)i/temporalResolution);
+            commandBuffer.Blit(renderTextures[i],BuiltinRenderTextureType.CameraTarget,maskMaterial);//the target is the screen
+        }
+    }
+}
---
+++ maskBlit.shader
@@ -0,0 +1,57 @@
+﻿Shader "Unlit/maskBlit"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+        _MaskTex ("Mask", 2D) = "white" {}
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            sampler2D _MainTex;
+            sampler2D _MaskTex;
+            float4 _MainTex_ST;
+            float _MaskPercentage;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                //fixed4 col = tex2D(_MaskTex, i.uv);
+                //if (col.r < _MaskPercentage)//commented out to avoid issues with 8 bit per channel textures, could be fixed by using higher precision textures
+                if (i.uv.y<_MaskPercentage)
+                    discard;
+                return tex2D(_MainTex, i.uv);
+            }
+            ENDCG
+        }
+    }
+}
